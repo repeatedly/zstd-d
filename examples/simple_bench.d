dbbb75ff40c26b3ffab7f85d1fb9c27587922427
Masahiro Nakagawa <repeatedly@gmail.com>
2016-09-05 10:50:55 +0900

    Add simple API bench

---
+++ examples/simple_bench.d
@@ -0,0 +1,33 @@
+static import std.file;
+import std.stdio;
+import std.datetime;
+import std.typetuple;
+
+void main(string[] args)
+{
+    auto source = std.file.read(args[1]);
+
+    void zlib()
+    {
+        import std.zlib;
+
+        auto r1 = compress(source, 1);
+        auto r2 = uncompress(r1);
+    }
+
+    void zstd()
+    {
+        import zstd.zstd;
+
+        auto r1 = compress(source, 1);
+        auto r2 = uncompress(r1);
+    }
+
+    alias TypeTuple!(zlib, zstd) Funcs;
+
+    immutable num = 10000;
+    auto times = benchmark!(Funcs)(num);
+    writeln(num, " loops (msecs) : smaller is better");
+    writeln("zlib: ", times[0].msecs);
+    writeln("zstd: ", times[1].msecs);
+}
