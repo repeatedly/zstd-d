033ba65eb815d41de2576ff07fb1fdd7559b3c85
Masahiro Nakagawa <repeatedly@gmail.com>
2016-09-16 00:32:46 +0900

    Update simple.d example

--- examples/simple.d
+++ examples/simple.d
@@ -7,11 +7,12 @@ void main(string[] args)
     if (args.length <= 1)
         throw new Exception("Must specify filename");
 
-    // second argument is compression level between 1 - 22
     auto original = std.file.read(args[1]);
-    auto compressed = compress(original, 3);
+    auto compressed = compress(original, 3);  // second argument is compression level between 1 - 22
+
     std.stdio.writefln("original size:   %d", original.length);
     std.stdio.writefln("compressed size: %d", compressed.length);
+
     auto uncompressed = uncompress(compressed);
 
     assert(original == uncompressed, "uncompression failed");
